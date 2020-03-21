6e3e2e51ea4755ee964b46eb38c230ff27a369b3
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-21 04:46:08 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,7 +4,7 @@
 # in order to complete the rest of this course and be ready for the next course!
 
 def introduction(name)
-  puts "Hi, my name is #{name}"
+  puts "Hi, my name is #{name}."
 end
 
 def introduction_with_language(name, language)
