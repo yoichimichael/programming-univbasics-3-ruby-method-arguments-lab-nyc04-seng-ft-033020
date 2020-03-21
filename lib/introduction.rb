0f353c501c6a68148adad275636e8a3db96cfb33
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-21 04:44:00 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,7 +11,8 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-
+def introduction_with_language_optional(name, language = "Ruby")
+end
 
 
 
